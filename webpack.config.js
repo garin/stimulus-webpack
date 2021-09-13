1ef89cd11a51af9e3213b74ed9460abf15be9bf1
yoshino <yoshino@tejimaya.com>
2021-09-13 09:43:59 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'development',
+
+  // メインとなる JavaScript ファイル（エントリーポイント）
+  entry: './src/application.js',
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/`,
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
+  devServer: {
+  }
+};
